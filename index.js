903112a4ad52b7bed12511cc025de4791c078982
Walter Krivanek <walter@vividvisions.com>
2024-08-10 18:44:15 +0200

    Package now exports _async.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { _ } from '#lib/tag.js';
+export { _, _async } from '#lib/tag.js';
 export { _if, _elseif, _else, _endif } from '#lib/syntax-if.js';
 export { _switch, _case, _default, _endswitch } from '#lib/syntax-switch.js';
 export { _always } from '#lib/syntax-always.js';
